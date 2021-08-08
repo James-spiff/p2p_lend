0a0eff7b2f06e31fff0abc4a1db8ab2735434649
James <Ebi James>
2021-08-08 17:12:09 +0100

    Frontend Implementation

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	return render(request, 'pages/home.html')
+
+def borrow(request):
+	return render(request, 'pages/borrow.html')
+
+def invest(request):
+	return render(request, 'pages/invest.html')
+
+def about_us(request):
+	return render(request, 'pages/about.html')
+
+def teams(request):
+	return render(request, 'pages/home.html')
