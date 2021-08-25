d0de3026863075c0bc1d1482ec3365ea2af66c55
James-spiff <heybeejames@gmail.com>
2021-08-25 19:00:08 +0100

    Created UserAddress model, alteredcurrent/permanent address fields

--- p2p_lend/users/models.py
+++ p2p_lend/users/models.py
@@ -6,6 +6,65 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 import uuid
 from django.utils import timezone
+from locations.models import Country, State, City
+
+class UserAddress(models.Model):
+
+    #Gives our address a unique id
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        help_text=_("""The unique identifier of the instance this object belongs to.
+                    Mandatory, unless a new instance to create is given."""))
+
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.PROTECT,
+        verbose_name=_('User'),
+        help_text=_('The user that owns the adress'))
+
+    address_line_1 = models.CharField(
+        max_length=125,
+        verbose_name=_('Adress line 1'),
+        help_text=_('Adress line 1 of the user'))
+
+    address_line_2 = models.CharField(
+        max_length=125,
+        verbose_name=_('Adress line 2'),
+        blank=True, null=True,
+        help_text=_('Adress line 2 of the user'))
+
+    state = models.ForeignKey(
+        State,
+        on_delete=models.PROTECT,
+        verbose_name=_('State'),
+        help_text=_('State of residence'))
+
+    city = models.ForeignKey(
+        City,
+        on_delete=models.PROTECT,
+        verbose_name=_('City'),
+        help_text=_('City of residence'))
+
+    zip_post_code = models.CharField(
+        max_length=20,
+        verbose_name=_('Zip code'),
+        help_text=_('User zip code'))
+
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.PROTECT,
+        verbose_name=_('Country'),
+        help_text=_('Country of residence'))
+
+    class Meta:
+        verbose_name=_('User Adress')
+        verbose_name_plural=_('User Adresses')
+
+    def __str__(self):
+        return self.name
+
 
 
 class UserManager(BaseUserManager):
@@ -88,16 +147,20 @@ class User(AbstractUser):
         unique=True,
         help_text=_("Email address of the client."))
 
-    current_adress = models.CharField(
+    current_adress = models.ForeignKey(
+        UserAddress,
+        on_delete=models.PROTECT,
         verbose_name=_('Current Adress'),
-        max_length=225,
         blank=True, null=True,
+        related_name='+', #this stops it from clashing with permanent address
         help_text=_("Current adress of the client."))
 
-    permanent_adress = models.CharField(
+    permanent_adress = models.ForeignKey(
+        UserAddress,
+        on_delete=models.PROTECT,
         verbose_name=_('Permanent Adress'),
-        max_length=225,
         blank=True, null=True,
+        related_name='+',
         help_text=_("Permanent adress of the client."))
 
     contact_number = models.CharField(
@@ -170,10 +233,11 @@ class User(AbstractUser):
         editable=False,
         help_text=_("Client's ip address recorded at the time of registration."))
 
-    country_of_residence = models.CharField(
+    country_of_residence = models.ForeignKey(
+        Country,
         verbose_name=_('Country of Residence'),
-        max_length=125,
         blank=True, null=True,
+        on_delete=models.SET_NULL,
         help_text=_("Client's country of residence. KYC verification will be applied to this country with proof of residence."))
 
     job_title = models.CharField(
